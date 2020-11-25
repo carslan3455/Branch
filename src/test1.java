3a60d9fc2f04c2e5e2209455a2adf6a5d142b78b
carslan3455 <carslan3455@gmail.com>
2020-11-25 20:54:30 +0100

    Sepete ekle islemi yapildi

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
 
         System.out.println("Ürünleri listele kismi yazildi");
+        System.out.println("Sepete ekle kismi yapildi");
     }
 }
